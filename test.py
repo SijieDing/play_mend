544bc285f0ee5776a00d1716494cfcd9fb0d3f0c
ericd <sijie.ding@ibm.com>
2026-01-08 19:56:20 -0500

    Try again

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-import os
-print("dfsdfsdfsdf")
+import bjoern
+def run_bjoern_server(app, host='127.0.0.1', port=8000):
+    """Run the Bjoern server in a separate thread."""
+    try:
+        bjoern.run(app, host, port, reuse_port=True)
+    except KeyboardInterrupt:
+        # Handle Ctrl+C within the server thread
+        print("Server interrupted, shutting down...")
+    finally:
+        print("Bjoern server stopped") 
+run_bjoern_server(None)
